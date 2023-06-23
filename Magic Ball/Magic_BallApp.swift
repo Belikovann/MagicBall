349bf62d148da38ca2f7f15a96ef8dfcba6cb9ee
@Annabel_ <hydrant94@gmail.com>
2023-06-23 17:11:25 +0200

    Initial Commit

---
+++ Magic Ball/Magic_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_BallApp.swift
+//  Magic Ball
+//
+//  Created by Аня Беликова on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Magic_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
